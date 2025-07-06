2d884d7c10704d03bc2d029ab08932fe69a8ca89
devopsnaresh87 <devopsnaresh87@gmail.com>
2025-07-06 18:38:32 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,94 +1,49 @@
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
 
-BG_BLACK=`tput setab 0`
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-BG_YELLOW=`tput setab 3`
-BG_BLUE=`tput setab 4`
-BG_MAGENTA=`tput setab 5`
-BG_CYAN=`tput setab 6`
-BG_WHITE=`tput setab 7`
 
-BOLD=`tput bold`
-RESET=`tput sgr0`
-#----------------------------------------------------start--------------------------------------------------#
+gcloud auth list
 
-echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 
-gcloud config set compute/zone $ZONE
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
 
-gsutil -m cp -r gs://spls/gsp053/orchestrate-with-kubernetes .
+export PROJECT_ID=$(gcloud config get-value project)
+
+gcloud config set compute/zone "$ZONE"
+
+gcloud container clusters create io --zone $ZONE
+
+gsutil -m cp -r gs://spls/gsp021/* .
 
 cd orchestrate-with-kubernetes/kubernetes
 
-gcloud container clusters create bootcamp \
-  --machine-type e2-small \
-  --num-nodes 1 \
-  --scopes "https://www.googleapis.com/auth/projecthosting,storage-rw"
+kubectl create deployment nginx --image=nginx:1.10.0
 
-sed -i 's/image: "kelseyhightower\/auth:2.0.0"/image: "kelseyhightower\/auth:1.0.0"/' deployments/auth.yaml
+kubectl expose deployment nginx --port 80 --type LoadBalancer
+
+cd ~/orchestrate-with-kubernetes/kubernetes
+kubectl apply -f pods/monolith.yaml
+kubectl create secret generic tls-certs --from-file tls/
+kubectl create configmap nginx-proxy-conf --from-file nginx/proxy.conf
+kubectl apply -f pods/secure-monolith.yaml
+
+kubectl apply -f services/monolith.yaml
+
+gcloud compute firewall-rules create allow-monolith-nodeport \
+  --allow=tcp:31000
+
+kubectl label pods secure-monolith 'secure=enabled'
+kubectl get pods secure-monolith --show-labels
 
 kubectl apply -f deployments/auth.yaml
-
-kubectl get deployments
-
-kubectl get pods
 
 kubectl apply -f services/auth.yaml
 
 kubectl apply -f deployments/hello.yaml
-
 kubectl apply -f services/hello.yaml
 
-kubectl create secret generic tls-certs --from-file tls/
 
 kubectl create configmap nginx-frontend-conf --from-file=nginx/frontend.conf
-
 kubectl apply -f deployments/frontend.yaml
-
 kubectl apply -f services/frontend.yaml
 
 kubectl get services frontend
-
-sleep 5
-
-kubectl scale deployment hello --replicas=5
-
-kubectl get pods | grep hello- | wc -l
-
-kubectl scale deployment hello --replicas=3
-
-kubectl get pods | grep hello- | wc -l
-
-sed -i 's/image: "kelseyhightower\/auth:1.0.0"/image: "kelseyhightower\/auth:2.0.0"/' deployments/hello.yaml
-
-kubectl get replicaset
-
-kubectl rollout history deployment/hello
-
-kubectl get pods -o jsonpath --template='{range .items[*]}{.metadata.name}{"\t"}{"\t"}{.spec.containers[0].image}{"\n"}{end}'
-
-kubectl rollout resume deployment/hello
-
-kubectl rollout status deployment/hello
-
-kubectl rollout undo deployment/hello
-
-kubectl rollout history deployment/hello
-
-kubectl get pods -o jsonpath --template='{range .items[*]}{.metadata.name}{"\t"}{"\t"}{.spec.containers[0].image}{"\n"}{end}'
-
-kubectl apply -f deployments/hello-canary.yaml
-
-kubectl get deployments
-
-echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
-
-#-----------------------------------------------------end----------------------------------------------------------#
