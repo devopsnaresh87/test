6ddc3e13518a693b19504812eeebaf3582c9c1a6
devopsnaresh87 <devopsnaresh87@gmail.com>
2025-07-06 15:29:54 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -25,15 +25,15 @@ RESET=`tput sgr0`
 
 echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
 
-gcloud beta container clusters create gmp-cluster --num-nodes=1 --zone $ZONE --enable-managed-prometheus
+gcloud beta container clusters create gmp-cluster --num-nodes=1 --zone $ZONE --enable-managed-prometheus --quiet
 
-gcloud container clusters get-credentials gmp-cluster --zone=$ZONE
+gcloud container clusters get-credentials gmp-cluster --zone=$ZONE --quiet
 
-kubectl create ns gmp-test
+kubectl create ns gmp-test --quiet || true
 
-kubectl -n gmp-test apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/prometheus-engine/v0.2.3/examples/example-app.yaml
+kubectl -n gmp-test apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/prometheus-engine/v0.2.3/examples/example-app.yaml --quiet &
 
-kubectl -n gmp-test apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/prometheus-engine/v0.2.3/examples/pod-monitoring.yaml
+kubectl -n gmp-test apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/prometheus-engine/v0.2.3/examples/pod-monitoring.yaml --quiet & wait
 
 git clone https://github.com/GoogleCloudPlatform/prometheus && cd prometheus
 
@@ -44,7 +44,7 @@ wget https://storage.googleapis.com/kochasoft/gsp1026/prometheus
 export PROJECT_ID=$(gcloud config get-value project)
 
 ./prometheus \
-  --config.file=documentation/examples/prometheus.yml --export.label.project-id=$PROJECT_ID --export.label.location=$ZONE 
+  --config.file=documentation/examples/prometheus.yml --export.label.project-id=$PROJECT_ID --export.label.location=$ZONE & 
 
 wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
 
@@ -54,7 +54,7 @@ cd node_exporter-1.3.1.linux-amd64
 
 cat > config.yaml <<EOF_END
 global:
-  scrape_interval: 5s
+  scrape_interval: 15s
 
 scrape_configs:
   - job_name: node
@@ -64,7 +64,7 @@ scrape_configs:
 EOF_END
 
 export PROJECT=$(gcloud config get-value project)
-gsutil mb -p $PROJECT gs://$PROJECT
+gsutil mb -p $PROJECT gs://$PROJECT --quiet
 gsutil cp config.yaml gs://$PROJECT
 gsutil -m acl set -R -a public-read gs://$PROJECT
 
